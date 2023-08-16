6cb8f12f8ce9066b10b7f532a44a0999c6aed40a
Andrie <andrei@disland.uk.to>
2023-08-16 14:49:41 +0000

    Don forget to conmit gays

---
+++ eagos/src/Loading.tsx
@@ -0,0 +1,9 @@
+import { AiOutlineLoading } from 'react-icons/ai';
+
+export default function Loading() {
+	return (
+		<div className='h-full grid relative top-[46vh] place-content-center'>
+			<AiOutlineLoading />
+		</div>
+	);
+}
